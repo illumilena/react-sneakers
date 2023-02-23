ea35bca3d58fcce6fc6182e3dbd0f01ac3daa59f
illumilena <kewwy@bk.ru>
2023-02-23 21:44:58 +0500

    Update price

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <ul className="d-flex">
           <li className="mr-30">
             <img width={18} height={18} src="/img/cart.svg"></img>
-            <span>1205 руб.</span>
+            <span>1206 руб.</span>
           </li>
           <li>
             <img width={18} height={18} src="/img/user.svg"></img>
